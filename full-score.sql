ca2bd577154184e4c054336f2cf861c8d29e2481
Rahul Rahul <144648310+Rahul272hshdhd@users.noreply.github.com>
2023-09-15 10:44:30 +0530

    Update full-score.sql
    https://github.com/charles-wangkai/hackerrank.wiki.git

--- full-score.sql
+++ full-score.sql
@@ -5,7 +5,7 @@ SELECT h.hacker_id, h.name
   JOIN Difficulty d
     ON d.difficulty_level = c.difficulty_level
    AND d.score = s.score
-  JOIN Hackers h
+  JOIN Hackers live 
     ON h.hacker_id = s.hacker_id
 GROUP BY h.hacker_id, h.name
 HAVING COUNT(*) > 1
